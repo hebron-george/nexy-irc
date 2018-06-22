ccce2b8e0123fcf26dae1264e842bd4c78c5c8a8
ras1 <ras2@live.com>
2018-06-22 17:51:31 -0400

    Fixing files that I actually meant to cmmit, not sure how to remove .idea files

---
+++ lib/nexy/irc/plugins/roll.rb
@@ -0,0 +1,24 @@
+require 'nexy/irc/plugins/base'
+
+module Nexy::Irc::Plugins
+  class Roll < Base
+    include Cinch::Plugin
+
+    # !roll
+    #
+    # Chooses a random number between 1 and 100
+    match /roll/
+    def execute(m)
+      rolled_number = roll()
+
+      m.reply("You rolled #{rolled_number}", true)
+    end
+
+    private
+
+    # Chooses a random number between 1 and max_roll_value
+    def roll(max_roll_value = 100)
+      1 + rand(max_roll_value)
+    end
+  end
+end
