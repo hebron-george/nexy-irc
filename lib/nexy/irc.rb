da44f011ee9827321cd1f96333915d82b17181b4
Hebron George <hebrontgeorge@gmail.com>
2018-07-01 16:51:54 -0500

    use config instead of hard coding

--- lib/nexy/irc.rb
+++ lib/nexy/irc.rb
@@ -6,36 +6,50 @@ require 'nexy/irc/plugins'
 module Nexy
   module Irc
     class Bot
+      attr_reader :bot, :bot_config
+
       def initialize
         puts 'Creating a new bot'
         require_relative '../../config/initializers/database'
 
         Nexy::Irc::Initialize::Database.set_up
+        @bot_config = bot_config
+        @bot = create_bot
       end
 
       def start
+        bot.start
+      end
+
+      private
+
+      def create_bot
         plugins = get_plugins
-        bot = Cinch::Bot.new do
+        config = bot_config
+        Cinch::Bot.new do
           configure do |c|
-            c.server   = 'irc.freenode.net'
-            # c.port     = '6669'
-            c.channels = ['#nexy']
-            c.nick     = 'nexy'
-            c.realname = 'nexy'
-            c.user     = 'nexy'
+            c.server   = config[:server]
+            c.port     = config[:port]
+            c.ssl.use  = config[:ssl] == 'true'
+            c.channels = config[:channels]
+            c.nick     = config[:nick]
+            c.realname = config[:realname]
+            c.user     = config[:user]
             c.plugins.plugins = plugins
-            # c.ssl.use  = true
           end
 
           on :message, "hello" do |m|
             m.reply "Hello, World"
           end
         end
-
-        bot.start
       end
 
-      private
+      def bot_config
+        @bot_config ||= begin
+          require_relative '../../config/config'
+          Nexy::Irc::Config.bot_config
+        end
+      end
 
       def get_plugins
         plugin_names = Nexy::Irc::Plugins.constants - [:Base] # Ignore the base plugin class
