1b450488ab4764becc1825449ccaf12c1783cb7c
Hebron George <hebrontgeorge@gmail.com>
2018-07-02 23:34:31 -0500

    require privileged plugins to implement user level

--- lib/nexy/irc/plugins/privileged/base.rb
+++ lib/nexy/irc/plugins/privileged/base.rb
@@ -2,6 +2,22 @@ require 'nexy/irc/plugins/base'
 
 module Nexy::Irc::Plugins::Privileged
   class Base < Nexy::Irc::Plugins::Base
+
+    # Each privileged plugin must implement
+    # this method, which returns the minimum
+    # user level required to be able to carry
+    # out the execution of the plugin.
+    #
+    # Possible user levels (highest to lowest)
+    #   :admin
+    #   :high
+    #   :medium
+    #   :low
+    #   :anyone
+    def user_level
+      raise NotImplementedError
+    end
+
     def authorized_user?(user)
       # require 'pry'; binding.pry
       user.authname && user.authname.to_s == 'test_auth_name'
