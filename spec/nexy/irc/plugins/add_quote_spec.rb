4624ce575e17482d7380b1655108946c9c73d83f
Hebron George <hebrontgeorge@gmail.com>
2018-07-09 20:28:41 -0500

    tests for addquote plugin

--- spec/nexy/irc/plugins/add_quote_spec.rb
+++ spec/nexy/irc/plugins/add_quote_spec.rb
@@ -5,16 +5,22 @@ RSpec.describe Nexy::Irc::Plugins::AddQuote do
 
   let(:bot)    { make_bot(described_class) }
   let(:plugin) { bot.plugins.first }
-  describe 'command pattern' do
+  describe 'for different messages' do
     subject { make_message(bot, message) }
 
-    context 'matches !addquote hello world' do
+    context 'good quote' do
       let(:message) { '!addquote hello world' }
-      it { expect(subject.message).to eq '!addquote hello world' }
+      it 'gets added' do
+        expect(subject.message).to eq '!addquote hello world'
+        expect(get_replies(subject).first.text).to include 'Added new quote!'
+      end
     end
-  end
 
-  describe 'command execution' do
-
+    context 'bad quote' do
+      let(:message) { '.!addquote will not match' }
+      it 'does not get added' do
+        expect(get_replies(subject)).to eq []
+      end
+    end
   end
 end
